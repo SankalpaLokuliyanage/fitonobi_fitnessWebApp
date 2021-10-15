4b63673a7ea6f6f8681fa61a46731ef941484265
SankalpaLokuliyanage <c.s.lokuliyanage@wlv.ac.uk>
2021-10-15 14:43:58 +0530

    create header - navigation bar

---
+++ header.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.80.0">
+    
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/carousel/">
+
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+
+
+  body {
+  padding-top: 3rem;
+  padding-bottom: 3rem;
+  color: #5a5a5a;
+}
+    </style>
+
+  </head>
+  <body>
+    
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+  <a class="navbar-brand" href="index.php"><img src="img/logo.png" width="200px"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav m-auto">
+      <li class="nav-item">
+        <a class="nav-link" style="font-size: 40px; padding-right: 30px; font-weight: bold; font-family: 'Times New Roman', Times, serif;" href="index.php"> 
+          HOME</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" style="font-size: 40px; padding-right: 30px; font-weight: bold; font-family: 'Times New Roman', Times, serif;" href="#">ABOUT US</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" style="font-size: 40px; padding-right: 30px; font-weight: bold; font-family: 'Times New Roman', Times, serif;" href="#">ARTICLES</a>
+      </li>
+    </ul>
+    <form class="d-flex">
+        <a href="#"><button type="button" style="margin-right:10px; margin-top: 10px; margin-bottom: 10px" class="btn btn-outline-light">CONTACT US</button></a>
+    </form>
+  </div>
+</nav>
+
+
+
