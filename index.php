fc34a3112e59ce36a5a54a9815471b9ad5c1a22d
SankalpaLokuliyanage <c.s.lokuliyanage@wlv.ac.uk>
2021-10-15 15:24:18 +0530

    add carousal to index.php

--- index.php
+++ index.php
@@ -1,3 +1,68 @@
 <?php
   require 'header.php';
 ?>
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+<div class="carousel-indicators">
+	<button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="img/img_caro1.jpg" style="filter: opacity(50%);" alt="First slide">
+
+      <div>
+          <div class="carousel-caption text-start" style="color: black;">
+          	<h1>Join with us</h1>
+            <p>You can share fitness experience with us.</p>
+            <p><a class="btn btn-lg btn-primary" href="#">Sign up today</a></p>
+          </div>
+       </div>
+
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="img/img_caro2.jpg" style="filter: opacity(50%); alt="Second slide">
+       <div>
+          <div class="carousel-caption text-start">
+            <p><a class="btn btn-lg btn-primary" href="#">View Articles</a></p>
+          </div>
+       </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="img/img_caro3.jpg" style="filter: opacity(50%); alt="Third slide">
+       <div>
+          <div class="carousel-caption text-start">
+            <p><a class="btn btn-lg btn-primary" href="#">Image Gallery</a></p>
+          </div>
+       </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+  
+  <style>
+  	.carousel {
+  margin-bottom: 4rem;
+}
+
+/* Declare heights because of positioning of img element */
+.carousel-item {
+  height: 32rem;
+}
+.carousel-item > img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  min-width: 100%;
+  height: 32rem;
+}
+  </style>
